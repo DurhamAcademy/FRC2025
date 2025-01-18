6db09a1ccdf13a56400481720ea624a8c18869db
Ryan Bauroth <25bauroth@da.org>
2025-01-17 21:06:05 -0500

    auto fomratting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,7 +12,7 @@
 // GNU General Public License for more details.
 
 package frc.robot;
-
+// testing formatting
 import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
