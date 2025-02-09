ba2831de546a71383e7da86ffae8524f960b7aa6
Ryan Bauroth <25bauroth@da.org>
2025-02-09 15:56:34 -0500

    fixed default command bug

--- src/main/java/frc/robot/commands/ElevatorCommands.java
+++ src/main/java/frc/robot/commands/ElevatorCommands.java
@@ -11,14 +11,16 @@ public class ElevatorCommands {
         return Commands.run(
                 () -> {
                     elevator.setTargetHeight(targetLevel);
-                });
+                },
+                elevator);
     }
 
     public static Command moveElevator(Elevator elevator, double power) {
         return Commands.run(
                 () -> {
                     elevator.setPower(power);
-                });
+                },
+                elevator);
     }
 
     public static Command zeroElevator(Elevator elevator) {
