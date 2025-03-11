256678fe274ba7c05a73ec52fc419cc04114086f
valenmoore <valen.h.moore@gmail.com>
2025-03-11 12:26:04 -0400

    deleted some sim stuff

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -2,9 +2,6 @@ package frc.robot.subsystems.intake;
 
 import edu.wpi.first.math.geometry.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import org.ironmaple.simulation.SimulatedArena;
-import org.ironmaple.simulation.gamepieces.GamePieceProjectile;
-import org.ironmaple.simulation.seasonspecific.reefscape2025.ReefscapeCoralOnFly;
 import org.littletonrobotics.junction.Logger;
 
 public class Intake extends SubsystemBase {
@@ -25,31 +22,6 @@ public class Intake extends SubsystemBase {
 
     public void stopMotors() {
         io.stopMotors();
-    }
-
-    private static boolean simInsideIntakeRange(
-            Pose2d simulatedDriveTrainPose, Pose3d coralPositionInAir) {
-        Translation3d robotPositionOnField =
-                new Translation3d(simulatedDriveTrainPose.getTranslation());
-        Rotation3d robotOrientation = new Rotation3d(simulatedDriveTrainPose.getRotation());
-        Translation3d intakePositionOnField =
-                robotPositionOnField.plus(
-                        IntakeConstants.intakePositionOnRobot.rotateBy(robotOrientation));
-
-        Translation3d difference = coralPositionInAir.getTranslation().minus(intakePositionOnField);
-        return Math.abs(difference.getX()) < IntakeConstants.intakeRange.getX()
-                && Math.abs(difference.getY()) < IntakeConstants.intakeRange.getY()
-                && Math.abs(difference.getZ()) < IntakeConstants.intakeRange.getZ();
-    }
-
-    public void simCheckForCoral(Pose2d robotPose) {
-        for (GamePieceProjectile gp : SimulatedArena.getInstance().gamePieceLaunched()) {
-            if (gp instanceof ReefscapeCoralOnFly) {
-                if (simInsideIntakeRange(robotPose, gp.getPose3d())) {
-                    SimulatedArena.getInstance().removeProjectile(gp);
-                }
-            }
-        }
     }
 
     @Override
