bb3a8d0f4fec96bc0e5c9877b351a9df22eabd1e
valenmoore <valen.h.moore@gmail.com>
2025-03-26 15:48:52 -0400

    vision working

--- src/main/java/frc/robot/subsystems/drive/Vision.java
+++ src/main/java/frc/robot/subsystems/drive/Vision.java
@@ -10,6 +10,7 @@ import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.*;
+import org.littletonrobotics.junction.Logger;
 import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
@@ -22,15 +23,25 @@ import org.photonvision.targeting.PhotonTrackedTarget;
 public class Vision extends SubsystemBase {
     private final SwerveDrivePoseEstimator poseEstimator;
 
-    Map<String, Transform3d> cameraTransforms = new HashMap<>(); // key: camera name, value: camera to robot transform
-    Map<PhotonCamera, PhotonPoseEstimator> cameraPoseEstimators = new HashMap<>(); // key: camera, value: pose estimator
+    Map<String, Transform3d> cameraTransforms =
+            new HashMap<>(); // key: camera name, value: camera to robot transform
+    Map<PhotonCamera, PhotonPoseEstimator> cameraPoseEstimators =
+            new HashMap<>(); // key: camera, value: pose estimator
 
     boolean useOnlyFrontCameras = false;
 
     private static final Matrix<N3, N1> kSingleTagStdDevs =
-            VecBuilder.fill(0.5, 0.5, Math.toRadians(10)); // single tag uncertainty: x and y can be off 50cm, rotation can be off 10 deg
+            VecBuilder.fill(
+                    0.5,
+                    0.5,
+                    Math.toRadians(
+                            10)); // single tag uncertainty: x and y can be off 50cm, rotation can
+    // be off 10 deg
     private static final Matrix<N3, N1> kMultiTagStdDevs =
-            VecBuilder.fill(0.2, 0.2, Math.toRadians(3)); // multi tag uncertainty: more confident for multiple tags
+            VecBuilder.fill(
+                    0.2,
+                    0.2,
+                    Math.toRadians(3)); // multi tag uncertainty: more confident for multiple tags
 
     public Vision(SwerveDrivePoseEstimator poseEstimator) {
         this.poseEstimator = poseEstimator;
@@ -38,19 +49,21 @@ public class Vision extends SubsystemBase {
         // cameras on the front should start with "front"
         cameraTransforms.put("front-camera", new Transform3d(0, 0, 0, new Rotation3d(0, 0, 0)));
 
-        AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.kDefaultField);
+        AprilTagFieldLayout aprilTagFieldLayout =
+                AprilTagFieldLayout.loadField(AprilTagFields.k2025ReefscapeWelded);
         for (String cameraName : cameraTransforms.keySet()) {
             Transform3d robotToCam = cameraTransforms.get(cameraName);
             try {
                 PhotonCamera camera = new PhotonCamera(cameraName);
 
                 // Construct PhotonPoseEstimator
-                PhotonPoseEstimator photonPoseEstimator = new PhotonPoseEstimator(
-                        aprilTagFieldLayout,
-                        PhotonPoseEstimator.PoseStrategy.CLOSEST_TO_REFERENCE_POSE,
-                        robotToCam
-                );
-                photonPoseEstimator.setMultiTagFallbackStrategy(PhotonPoseEstimator.PoseStrategy.LOWEST_AMBIGUITY);
+                PhotonPoseEstimator photonPoseEstimator =
+                        new PhotonPoseEstimator(
+                                aprilTagFieldLayout,
+                                PhotonPoseEstimator.PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR,
+                                robotToCam);
+                photonPoseEstimator.setMultiTagFallbackStrategy(
+                        PhotonPoseEstimator.PoseStrategy.LOWEST_AMBIGUITY);
 
                 cameraPoseEstimators.put(camera, photonPoseEstimator);
             } catch (Exception e) {
@@ -62,10 +75,12 @@ public class Vision extends SubsystemBase {
     /**
      * The latest estimated robot pose on the field from vision data. This may be empty. This should
      * only be called once per loop.
+     *
      * @return An {@link EstimatedRobotPose} with an estimated pose, estimate timestamp, and targets
      *     used for estimation.
      */
-    public Optional<EstimatedRobotPose> getEstimatedGlobalPose(PhotonCamera camera, PhotonPoseEstimator photonEstimator) {
+    public Optional<EstimatedRobotPose> getEstimatedGlobalPose(
+            PhotonCamera camera, PhotonPoseEstimator photonEstimator) {
         Optional<EstimatedRobotPose> visionEst = Optional.empty();
         for (var change : camera.getAllUnreadResults()) {
             visionEst = photonEstimator.update(change);
@@ -75,9 +90,10 @@ public class Vision extends SubsystemBase {
     }
 
     /**
-     * Calculates new standard deviations This algorithm is a heuristic that creates dynamic standard
-     * deviations based on number of tags, estimation strategy, and distance from the tags.
-     * from <a href="https://github.com/PhotonVision/photonvision/blob/main/photonlib-java-examples/poseest/src/main/java/frc/robot/Vision.java">this</a>
+     * Calculates new standard deviations This algorithm is a heuristic that creates dynamic
+     * standard deviations based on number of tags, estimation strategy, and distance from the tags.
+     * from <a
+     * href="https://github.com/PhotonVision/photonvision/blob/main/photonlib-java-examples/poseest/src/main/java/frc/robot/Vision.java">this</a>
      *
      * @param estimatedPose The estimated pose to guess standard deviations for.
      * @param targets All targets in this camera frame
@@ -101,11 +117,15 @@ public class Vision extends SubsystemBase {
                 if (tagPose.isEmpty()) continue;
                 numTags++;
                 avgDist +=
-                        tagPose
-                                .get()
+                        tagPose.get()
                                 .toPose2d()
                                 .getTranslation()
-                                .getDistance(estimatedPose.get().estimatedPose.toPose2d().getTranslation());
+                                .getDistance(
+                                        estimatedPose
+                                                .get()
+                                                .estimatedPose
+                                                .toPose2d()
+                                                .getTranslation());
             }
 
             if (numTags == 0) {
@@ -118,7 +138,8 @@ public class Vision extends SubsystemBase {
                 if (numTags > 1) estStdDevs = kMultiTagStdDevs;
                 // Increase std devs based on (average) distance
                 if (numTags == 1 && avgDist > 4)
-                    estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+                    estStdDevs =
+                            VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
                 else estStdDevs = estStdDevs.times(1 + (avgDist * avgDist / 30));
                 return estStdDevs;
             }
@@ -126,18 +147,29 @@ public class Vision extends SubsystemBase {
     }
 
     private void updateEstimatedPose() {
+        Logger.recordOutput("Vision/running", true);
         for (PhotonCamera camera : cameraPoseEstimators.keySet()) {
-            if (useOnlyFrontCameras && !camera.getName().contains("front")) {
+            /*if (useOnlyFrontCameras && !camera.getName().contains("front")) {
                 // skip camera because it's not on the front
                 continue;
-            }
+            }*/
             PhotonPoseEstimator photonPoseEstimator = cameraPoseEstimators.get(camera);
-            Optional<EstimatedRobotPose> estimatedPose = getEstimatedGlobalPose(camera, photonPoseEstimator);
+            Optional<EstimatedRobotPose> estimatedPose =
+                    getEstimatedGlobalPose(camera, photonPoseEstimator);
             if (estimatedPose.isPresent()) {
+                //                Logger.recordOutput("Vision/cameraOutput", true);
                 Pose2d pose = estimatedPose.get().estimatedPose.toPose2d();
+                Logger.recordOutput("Vision/cameraOutput", pose);
                 double timestamp = estimatedPose.get().timestampSeconds;
-                Matrix<N3, N1> stdDevs = calculateEstimationStdDevs(estimatedPose, estimatedPose.get().targetsUsed, photonPoseEstimator);
-                poseEstimator.addVisionMeasurement(pose, timestamp, stdDevs); // taking in curStdDevs to apply confidence
+                Matrix<N3, N1> stdDevs =
+                        calculateEstimationStdDevs(
+                                estimatedPose,
+                                estimatedPose.get().targetsUsed,
+                                photonPoseEstimator);
+                poseEstimator.addVisionMeasurement(
+                        pose, timestamp, stdDevs); // taking in curStdDevs to apply confidence
+            } else {
+                //                Logger.recordOutput("Vision/cameraOutput", false);
             }
         }
     }
