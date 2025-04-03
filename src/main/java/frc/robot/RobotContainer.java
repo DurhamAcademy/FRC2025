4debd8258954b5066459a8534bd75b20bc4e5b17
Ryan Bauroth <25bauroth@da.org>
2025-04-03 15:25:12 -0400

    deleted test thing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -147,27 +147,27 @@ public class RobotContainer {
         // Set up auto routines
         autoChooser = new LoggedDashboardChooser<>("Auto Chooser", AutoBuilder.buildAutoChooser());
 
-        autoChooser.addOption(
-                "Drive Wheel Radius Characterization",
-                DriveCommands.wheelRadiusCharacterization(drive));
-        autoChooser.addOption(
-                "Drive Simple FF Characterization",
-                DriveCommands.feedforwardCharacterization(drive));
-        autoChooser.addOption(
-                "Drive SysId (Quasistatic Forward)",
-                drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-        autoChooser.addOption(
-                "Drive SysId (Quasistatic Reverse)",
-                drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-        autoChooser.addOption(
-                "Drive SysId (Dynamic Forward)",
-                drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
-        autoChooser.addOption(
-                "Drive SysId (Dynamic Reverse)",
-                drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+    //        autoChooser.addOption(
+    //                "Drive Wheel Radius Characterization",
+    //                DriveCommands.wheelRadiusCharacterization(drive));
+    //        autoChooser.addOption(
+    //                "Drive Simple FF Characterization",
+    //                DriveCommands.feedforwardCharacterization(drive));
+    //        autoChooser.addOption(
+    //                "Drive SysId (Quasistatic Forward)",
+    //                drive.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+    //        autoChooser.addOption(
+    //                "Drive SysId (Quasistatic Reverse)",
+    //                drive.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+    //        autoChooser.addOption(
+    //                "Drive SysId (Dynamic Forward)",
+    //                drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
+    //        autoChooser.addOption(
+    //                "Drive SysId (Dynamic Reverse)",
+    //                drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
 
-        // Configure the button bindings
-        sendDataToSmartDashboard();
+    // Configure the button bindings
+    sendDataToSmartDashboard();
         configureButtonBindings();
     }
 
